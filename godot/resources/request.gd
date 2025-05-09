5b21edd906b8ac8e53ff49dd5ce11125af7d8ed1
gregwoodsfrontier <iwannaplaygamesjp@gmail.com>
2025-05-09 15:58:19 -0400

    added world boundaries

---
+++ godot/resources/request.gd
@@ -0,0 +1,14 @@
+extends Resource
+class_name Request
+
+enum REQUEST_STAT {
+	QUEUE,
+	PROCESSING,
+	COMPLETE
+}
+
+@export var customer: String
+@export var food: String
+@export var wait_time: float
+@export var tip_value: int
+@export var status: REQUEST_STAT
--- godot/scenes/game_camera/game_camera.gd
+++ godot/scenes/game_camera/game_camera.gd
@@ -8,12 +8,12 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	acquire_target()
-	global_position = global_position.lerp(target_position, 1.0 - exp(-delta * 20))
-
-func acquire_target() -> void:
-	var player_nodes = get_tree().get_nodes_in_group("player")
-	if player_nodes.size() > 0:
-		var player : Node2D = player_nodes[0]
-		target_position = player.global_position
+#func _process(delta: float) -> void:
+	#acquire_target()
+	#global_position = global_position.lerp(target_position, 1.0 - exp(-delta * 20))
+#
+#func acquire_target() -> void:
+	#var player_nodes = get_tree().get_nodes_in_group("player")
+	#if player_nodes.size() > 0:
+		#var player : Node2D = player_nodes[0]
+		#target_position = player.global_position
